14d36038021883dc115289cc0e9046a1a63523a5
NuVotYTutEst <116557107+NuVotYTutEst@users.noreply.github.com>
2023-04-05 13:07:31 +0400

    Add files via upload

--- kakulator.java
+++ kakulator.java
@@ -2,41 +2,76 @@ package tictactoe;
 import java.util.Scanner;
 public class kakulator {
 	 public static void main(String[] args) {
-		 Scaner(args);
-		 System.out.print(Otvet(a, b));
+		 Vvode;
+		 System.out.println(Otvet());
+		 
 	 }
-	 public static void Otvet(String[] args) {
-		 Scaner(args);
-		 char op = sc.next().charAt(0);
-	     vso(a, b, op);
-	 }
-	 public static void Scaner(String[] args) {
+	 public static double Vvode() {
 		 Scanner sc = new Scanner(System.in);
-		 System.out.print("Введите первое и второе число:");
-		 int a = sc.nextInt();
-	     int b = sc.nextInt();
-	     System.out.print("Введите тип операции которую хотите выполнить( +, -, *, /, % ):");
+		 System.out.print("Введите первое число:");
+		 double a = sc.nextDouble();
+		 System.out.print("Введите знак:");
+		 char op = sc.next().charAt(0);
+		 if (op == 's') {
+			 return op, a;
+		 }
+		 System.out.print("Введите второе число:");
+		 double b = sc.nextDouble();
+		 return op; 
 	 }
-	 public static int vso(int a, int b, char op) {
-		 int otvet = 0;
+//	 public static double schtoto() {
+//		 return Otvet(Pervoe(), Znak(), Vtoroe());
+//	 }
+	 public static double Otvet(double a, char op, double b) {
 		 if (op == '+'){
-			 otvet = a + b;
+			return plus(a,b);
 		 }
 		 else if (op == '-') {
-			 otvet = a - b;
+			 return minus(a,b);
 	        }
 	     else if (op == '*') {
-	    	 otvet = a * b;
+	    	 return umnoshitb(a,b);
 	        }
 	     else if (op == '/') {
-	    	 otvet = a / b;
+	    	 return delitb(a,b);
 	        }
 	     else if (op == '%') {
-	    	 otvet = a % b;
+	    	 return procent(a,b);
 	        }
-		 System.out.println("Ответ:" + otvet);
+	     else if (op == 's') {
+	     	 return sin(a);
+	        }
+		 return a;
+	 }
+	 public static double plus(double a, double b) {
+		 double otvet = 0;
+		 otvet = a + b;
+		 return otvet;
+	 }
+	 public static double minus(double a, double b) {
+		 double otvet = 0;
+		 otvet = a - b;
+		 return otvet;
+	 }
+	 public static double umnoshitb(double a, double b) {
+		 double otvet = 0;
+	 	 otvet = a * b;
+		 return otvet;
+	 }
+	 public static double delitb(double a, double b) {
+		 double otvet = 0;
+		 otvet = a / b;
+		 return otvet;
+	 }
+	 public static double procent(double a, double b) {
+		 double otvet = 0;
+		 otvet = a % b;
+		 return otvet;
+	 }
+	 public static double sin(double a) {
+		 double otvet = 0;
+		 otvet = Math.sin(Math.toRadians(a));
 		 return otvet;
 	 }
 }
-
 
